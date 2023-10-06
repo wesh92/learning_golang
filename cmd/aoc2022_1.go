746c0a95eb653691ceec59383f46c0f46ccd1004
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2023-10-05 22:31:48 -0400

    fixing names?

--- cmd/aoc2022_1.go
+++ cmd/aoc2022_1.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/wesh92/learning_golang/pkg/httpGet" // adjust this import path based on your module name
+	"github.com/wesh92/learning_golang/pkg/httpstuff/aocget" // adjust this import path based on your module name
 )
 
 func main() {
-	body, err := httpGet.Request("https://adventofcode.com/2020/day/1/input", "session=53616c7465645f5f8f8c62e3a9464a86199f1d08848653e924f0cacb73b0778c086e27eac56006b47dcdbcffa79a788cd30fecaa1c85547a3ee17f0cc731e304")
+	body, err := aocget.AoCRequest("https://adventofcode.com/2020/day/1/input", "session=53616c7465645f5f8f8c62e3a9464a86199f1d08848653e924f0cacb73b0778c086e27eac56006b47dcdbcffa79a788cd30fecaa1c85547a3ee17f0cc731e304")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
--- pkg/httpStuff/aocGet.go
+++ pkg/httpStuff/aocGet.go
@@ -1,4 +1,4 @@
-package aocGet
+package aocget
 
 import (
 	"fmt"
