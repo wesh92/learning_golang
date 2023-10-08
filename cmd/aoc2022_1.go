5d87d753ce3287905429c9e3ab7b47ee261f89b9
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2023-10-07 22:50:21 -0400

    AoC 2022 Day 1 complete!

--- cmd/aoc2022_1.go
+++ cmd/aoc2022_1.go
@@ -2,15 +2,80 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
 
 	aocget "github.com/wesh92/learning_golang/pkg/httpstuff"
 )
 
-func main() {
-	body, err := aocget.AoCRequest("https://adventofcode.com/2020/day/1/input", "session=53616c7465645f5f8f8c62e3a9464a86199f1d08848653e924f0cacb73b0778c086e27eac56006b47dcdbcffa79a788cd30fecaa1c85547a3ee17f0cc731e304")
+func getInput() (string, error) {
+	body, err := aocget.AoCRequest("https://adventofcode.com/2022/day/1/input", "session=")
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Println("Error:", err)
+		return "", err
+	}
+	return string(body), nil
+}
+
+func provideLines() ([]string, error) {
+	input, err := getInput()
+	if err != nil {
+		log.Println("Error:", err)
+		return nil, err
+	}
+	return strings.Split(strings.TrimSpace(input), "\n"), nil
+}
+
+type ElfUnit struct {
+	Sum   uint32
+	Index int
+}
+
+func sumUnitGroups() ([]ElfUnit, error) {
+
+	lines, err := provideLines()
+	if err != nil {
+		log.Println("Error:", err)
+		return nil, err
+	}
+
+	units := []ElfUnit{}
+	currentSum := 0
+	unitIndex := 0
+	for _, line := range lines {
+		if line == "" {
+			units = append(units, ElfUnit{uint32(currentSum), unitIndex})
+			currentSum = 0
+			unitIndex++
+			continue
+		}
+		num, err := strconv.ParseUint(line, 10, 32)
+		if err != nil {
+			log.Println("Error:", err)
+			return nil, err
+		}
+		currentSum += int(num)
+	}
+
+	if currentSum > 0 {
+		units = append(units, ElfUnit{uint32(currentSum), unitIndex})
+	}
+
+	return units, nil
+}
+
+func main() {
+	units, err := sumUnitGroups()
+	if err != nil {
+		log.Println("Error:", err)
 		return
 	}
-	fmt.Println(body)
+
+	sort.Slice(units, func(i, j int) bool {
+		return units[i].Sum > units[j].Sum
+	})
+
+	fmt.Println("Greatest Sum:", units[0].Sum, "for Unit:", units[0].Index)
 }
